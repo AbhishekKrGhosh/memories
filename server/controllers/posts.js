390447891c5667541a50aee1116cfa63fd0e18fd
Abhishek Kumar <abhishek.andri05@gmail.com>
2023-08-19 23:27:36 +0530

    get create and update added

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,6 +1,7 @@
+import mongoose from "mongoose"
 import PostMessage from "../models/postMessage.js"
 
-export const getPost = async(req,res)=>{
+export const getPosts = async(req,res)=>{
     try {
         const postMessages = await PostMessage.find()
         res.status(200).json(postMessages)
@@ -19,4 +20,12 @@ export const createPost = async(req, res)=>{
         res.status(409).json({message: error.message})
     }
 
+}
+export const updatePost = async(req,res)=>{
+    const {id : _id} = req.params
+    const post  = req.body
+    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id')
+
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, { new: true})
+    res.json(updatePost)
 }
