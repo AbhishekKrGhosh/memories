2a421d4eb24123f0c4ad6f25680a711044d056ce
Abhishek Kumar <abhishek.andri05@gmail.com>
2024-02-20 23:01:31 +0530

    deployment

--- server/index.js
+++ server/index.js
@@ -4,8 +4,7 @@ import mongoose from "mongoose";
 import cors from "cors"
 import dotenv from "dotenv"
 import postRoutes from './routes/posts.js'
-
-
+import path from 'path'
 dotenv.config()
 
 const app = express()
@@ -19,8 +18,24 @@ app.use('/posts', postRoutes)
 
 const username = process.env.UNAME
 const pwd = process.env.PWD
-const CONNECTION_URL = `mongodb+srv://${username}:${pwd}@cluster0.bifs2nr.mongodb.net/`
-const PORT = process.env.PORT || 5000
+const CONNECTION_URL = `mongodb+srv://${username}:${pwd}@cluster0.i7dbbcc.mongodb.net/`
+const PORT = 5000
+//---------------deployment--------------//
+
+const __dirname1 = path.resolve()
+if(process.env.NODE_ENV=== 'production'){
+    app.use(express.static(path.join(__dirname1,'/client/build')))
+    app.get('*', (req,res) => {
+        res.sendFile(path.resolve(__dirname1, 'client', 'build', 'index.html'))
+    })
+}
+else{
+    app.get('/', (req,res)=> {
+        res.send("running successfully")
+    })
+}
+
+//---------------deployment--------------//
 
 mongoose.connect(CONNECTION_URL)
 .then(()=>app.listen(PORT,()=>console.log(`Server running at PORT: ${PORT}`)))
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,9 +1,10 @@
 import express from "express"
 import {getPosts, createPost, updatePost} from '../controllers/posts.js'
-
+import path from 'path'
 const router = express.Router()
 
 router.get('/',getPosts)
 router.post('/',createPost)
 router.patch('/:id', updatePost)
+
 export default router
